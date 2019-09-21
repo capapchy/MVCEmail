46674e157afb9b5a8368e064a99c389f58cf2bb7
capapchy <capapchy@sina.com>
2019-09-21 09:41:25 +0800

    Add files via upload

---
+++ MailHelpr.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Web;
+
+namespace WMailService
+{
+    public static class MailHelper
+    {
+        /// <summary>
+        /// SMTP服务器地址
+        /// </summary>
+        public static string Smtp { get; set; }
+        /// <summary>
+        /// SMTP服务器端口号
+        /// </summary>
+        public static int SmtpPort { get; set; }
+        /// <summary>
+        /// IMAP服务器地址
+        /// </summary>
+        public static string Imap { get; set; }
+        /// <summary>
+        /// IMAP服务器地址端口号
+        /// </summary>
+        public static int ImapPort { get; set; }
+        /// <summary>
+        /// 系统发邮件地址
+        /// </summary>
+        public static string MailAddress { get; set; }
+        /// <summary>
+        /// 邮件密码
+        /// </summary>
+        public static string MailPassWord { get; set; }
+        /// <summary>
+        /// 设置邮件服务器
+        /// </summary>
+        /// <param name="Smtp">SMTP服务器地址</param>
+        /// <param name="SmtpPort">SMTP服务器端口号</param>
+        public static void SetServer(string smtp, int smtpport)
+        {
+            Smtp = smtp;
+            SmtpPort = smtpport;
+        }
+
+        /// <summary>
+        /// 设置邮件服务器
+        /// </summary>
+        /// <param name="Smtp">SMTP服务器地址</param>
+        /// <param name="SmtpPort">SMTP服务器端口号</param>
+        /// <param name="Imap">IMAP服务器地址</param>
+        /// <param name="ImapPort">IMAP服务器地址端口号</param>
+        public static void SetServer(string smtp,int smtpport,string imap,int imapport)
+        {
+            Smtp = smtp;
+            SmtpPort = smtpport;
+            Imap = imap;
+            ImapPort = imapport;
+        }
+        /// <summary>
+        /// 设置发件人地址
+        /// </summary>
+        /// <param name="mailaddress">发件人地址</param>
+        public static void SetMailAddress(string mailaddress,string password)
+        {
+            MailAddress = mailaddress;
+            MailPassWord = password;
+        }
+        /// <summary>
+        /// 设置邮件服务器地址，默认从客户服务器配置初始化，若客户未配置则通过服务器配置文件初始化
+        /// </summary>
+        public static void InitMailServer()
+        {
+            Smtp = ConfigurationManager.AppSettings["smtp"];
+            SmtpPort= int.Parse(ConfigurationManager.AppSettings["smtpport"]);
+            Imap = ConfigurationManager.AppSettings["imap"];
+            ImapPort = int.Parse(ConfigurationManager.AppSettings["imapport"]);
+            MailAddress = ConfigurationManager.AppSettings["mailaddress"];
+            MailPassWord = ConfigurationManager.AppSettings["mailpassword"];
+        }
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="args">邮件信息：JSON数组。如：[{"to":"收件人地址列表","cc":"抄送人地址列表","bcc":"密送人地址列表","subject":"邮件标题","body":"邮件内容","file":"附件地址"},……]</param>
+        public static dynamic SendMail( dynamic args,bool sl=false,int timeout=0)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = Smtp;
+            try
+            {
+                client.Port = SmtpPort;
+                if (SmtpPort == 465)
+                    sl = true;
+            }catch(Exception ex)
+            {
+                client.Port = 25;
+            }
+            client.EnableSsl = sl;
+            //client.Timeout = 18000;//timeout;
+
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            //验证发件人凭证
+            client.Credentials = new System.Net.NetworkCredential(MailAddress, MailPassWord);
+            MailMessage mm;
+            foreach (dynamic item in args)
+            {
+                mm = new MailMessage();
+                mm.From = new MailAddress(MailAddress);
+                string[] to = null ;
+                string[] cc=null;
+                string[] bcc=null;
+                string subject="";
+                string body="";
+                string[] SUpFile=null;
+                try
+                {
+                    Type type = item.GetType(); 
+                    if(type.Name=="JObject")
+                    {
+                        subject = item["subject"];
+                        body = item["body"];
+                        //设置发件人地址
+                        to = item["to"].ToString().Split(',');
+                        //设置抄送人地址
+                        cc = item["cc"].ToString().Split(',');
+                        //设置密送人地址
+                        bcc = item["bcc"].ToString().Split(',');
+                        //设置待发送文件
+                        SUpFile = item["file"].ToString().Split(',');
+                    }
+                    else
+                    {
+                        subject = item.subject;
+                        body = item.body;
+                        //设置发件人地址
+                        to = item.to.ToString().Split(',');  
+                        //设置抄送人地址
+                        cc = item.cc.ToString().Split(',');
+                        //设置密送人地址
+                        bcc = item.bcc.ToString().Split(',');
+                        SUpFile = item.file.ToString().Split(',');
+                    }
+                    //设置收件人地址
+                    foreach (string itemto in to)
+                    {
+                        if(!string.IsNullOrEmpty(itemto))
+                            mm.To.Add(itemto);
+                    }
+                    //设置抄送人
+                    foreach (string itemcc in cc)
+                    {
+                        if (!string.IsNullOrEmpty(itemcc))
+                            mm.CC.Add(itemcc);
+                    }
+                    //设置密送人
+                    foreach (string itembcc in bcc)
+                    {
+                        if (!string.IsNullOrEmpty(itembcc))
+                            mm.Bcc.Add(itembcc);
+                    }
+                    foreach(string file in SUpFile)
+                    {
+                        if (string.IsNullOrEmpty(file))
+                            continue;
+                        //将文件进行转换成Attachments
+                        Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
+                        // Add time stamp information for the file.
+                        ContentDisposition disposition = data.ContentDisposition;
+                        disposition.CreationDate = System.IO.File.GetCreationTime(file);
+                        disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
+                        disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+
+                        mm.Attachments.Add(data);
+                        System.Net.Mime.ContentType ctype = new System.Net.Mime.ContentType();
+                    }
+                    mm.Subject = subject;
+                    mm.Body = body;
+                    mm.BodyEncoding = UTF8Encoding.UTF8;
+                    mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                    client.Send(mm);
+                }catch(Exception ex)
+                {
+                    return new { status = false, message = ex.Message };
+                }
+            }
+            return new { status = true, message = "" };
+        }
+    }
+}
---
+++ WMailService.cs
@@ -0,0 +1,229 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMailService
+{
+    public partial class WMailService : Form
+    {
+        int readtime = 3000;
+        int sendtime = 60000;
+        string readapi = "";
+        string sendapi = "";
+        string completeapi = "";
+        int dailyreport = 0;
+        string dailyreportapi = "";
+        bool readsending = false;
+        bool sendstatus = true;
+        string path = Application.StartupPath + @"\Log\";
+        string LogFile="";
+        List<MailList> maillist = new List<MailList>();
+        TxtFileControl file = new TxtFileControl();
+        bool ssl = true;
+        static string today = "2018-04-19";
+        string MsgId = "";
+
+        private delegate void Serial_Data_Calback();
+        public WMailService()
+        {
+            InitializeComponent();
+        }
+
+        private void WMailService_Load(object sender, EventArgs e)
+        {
+            readtime = ConfigurationManager.AppSettings["readtime"]==""?3000:int.Parse(ConfigurationManager.AppSettings["readtime"])*1000;
+            sendtime = ConfigurationManager.AppSettings["sendtime"] == "" ? 60000 : int.Parse(ConfigurationManager.AppSettings["readtime"]) * 60000;
+            readsending = ConfigurationManager.AppSettings["checksending"] == "" ? false : bool.Parse(ConfigurationManager.AppSettings["checksending"]);
+            readapi = ConfigurationManager.AppSettings["getmailapi"];
+            sendapi= ConfigurationManager.AppSettings["sendingmailapi"]; 
+            completeapi= ConfigurationManager.AppSettings["sendedmailapi"];
+            dailyreport= int.Parse(ConfigurationManager.AppSettings["dailyreport"]);
+            dailyreportapi = ConfigurationManager.AppSettings["dailyreportapi"];
+            ssl = bool.Parse(ConfigurationManager.AppSettings["ssl"]);
+            MsgId = ConfigurationManager.AppSettings["MsgId"];
+            MailHelper.InitMailServer();
+            tbFrom.Text = MailHelper.MailAddress;
+            tbTo.Focus();
+            timer1.Enabled = true;
+            timer1.Interval = readtime;
+            timer2.Enabled = true;
+            timer2.Interval = sendtime;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private void btSendMail_Click(object sender, EventArgs e)
+        {
+            string mailfrom = tbFrom.Text;
+            string mailto = tbTo.Text;
+            string mailcc = tbcc.Text;
+            string mailbcc = tbbcc.Text;
+            string mailbody = mailBody.Text;
+            string mailsubject = tbSubject.Text;
+            if(!string.IsNullOrEmpty(mailto) && !string.IsNullOrEmpty(mailfrom)&&!string.IsNullOrEmpty(mailbody)&&!string.IsNullOrEmpty(mailsubject))
+            {
+                List<dynamic> list = new List<dynamic>();
+                list.Add(new { to=mailto,cc=mailcc,bcc=mailbcc, subject = mailsubject, body = mailbody,file=""});
+                dynamic sendlog = MailHelper.SendMail(list, ssl);
+                if (sendlog.status)
+                {
+                    if (sendMailList.Items.Count > 20)
+                    {
+                        sendMailList.Items.RemoveAt(sendMailList.Items.Count-1);
+                        sendMailList.Items.Insert(0, "from:" + mailfrom + "; to:" + mailto + "; cc:" + mailcc + "; bcc:" + mailbcc + "; subject:" + mailsubject + "; body:" + mailbody);
+                    }
+                    else
+                    {
+                        sendMailList.Items.Insert(0, "from:" + mailfrom + "; to:" + mailto + "; cc:" + mailcc + "; bcc:" + mailbcc + "; subject:" + mailsubject + "; body:" + mailbody);
+                    }
+                    tbTo.Text = "";
+                    tbcc.Text = "";
+                    tbbcc.Text = "";
+                    mailBody.Text = "";
+                    tbSubject.Text = "";
+                    tbTo.Focus();
+                }
+                else
+                {
+                    file.addLine(LogFile, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送失败:" + sendlog.message);
+                }
+            }
+        }
+
+        public void sendmail()
+        {
+            sendstatus = false;
+            try
+            {
+                ThreadPool.QueueUserWorkItem(h =>
+                {
+                    Control.CheckForIllegalCrossThreadCalls = false;
+
+                    Serial_Data_Calback sdc = new Serial_Data_Calback(like);
+                    this.Invoke(sdc);
+                    while (maillist.Count > 0)
+                    {
+                        MailList mail = maillist[0];
+                        dynamic data = mail.Data;
+                        string sendmessage = "from:" + MailHelper.MailAddress + "; to:" + data.MailTo + "; cc:" + data.CC + "; bcc:" + data.BCC + "; subject:" + data.Title + "; body:" + data.Body;
+                        List<dynamic> list = new List<dynamic>();
+                        list.Add(new { to = data.MailTo, cc = data.CC, bcc = data.BCC, subject = data.Title, body = data.Body, file = data.Source });
+                        dynamic sendlog = MailHelper.SendMail(list, ssl);
+                        if (sendlog.status)
+                        {
+                            var where = new { RowId = data.RowId };
+                            HttpHelper.PostWebApi(completeapi, JsonConvert.SerializeObject(where), 18000);
+                            if (sendMailList.Items.Count > 20)
+                            {
+                                sendMailList.Items.RemoveAt(sendMailList.Items.Count - 1);
+                                sendMailList.Items.Insert(0, sendmessage);
+                            }
+                            else
+                            {
+                                sendMailList.Items.Insert(0, sendmessage);
+                            }
+                            file.addLine(LogFile, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：" + sendmessage);
+                            maillist.Remove(mail);
+                        }
+                        else
+                        {
+                            file.addLine(LogFile, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送失败：RowId" + mail.RowId + ":" + data.MailTo);
+                            file.addLine(LogFile, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送失败:" + sendlog.message);
+                            if (sendMailList.Items.Count > 20)
+                            {
+                                sendMailList.Items.RemoveAt(sendMailList.Items.Count - 1);
+                                sendMailList.Items.Insert(0, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送失败：RowId" + mail.RowId + ":" + data.MailTo);
+                            }
+                            else
+                            {
+                                sendMailList.Items.Insert(0, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送失败：RowId" + mail.RowId + ":" + data.MailTo);
+                            }
+                            maillist.Remove(mail);
+                        }
+                        Thread.Sleep(5000);
+                    }
+                    sendstatus = true;
+                });
+            }catch(Exception ex)
+            {
+                file.addLine(LogFile, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：数据错误：" + ex.Message);
+                if (sendMailList.Items.Count > 20)
+                {
+                    sendMailList.Items.RemoveAt(sendMailList.Items.Count - 1);
+                    sendMailList.Items.Insert(0, ex.Message);
+                }
+                else
+                {
+                    sendMailList.Items.Insert(0, ex.Message);
+                }
+                sendstatus = true;
+            }
+        }
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if(DateTime.Now.Hour== dailyreport && today != DateTime.Now.ToString("yyyy-MM-dd"))
+            {
+                dynamic result = HttpHelper.GetWebApi(dailyreportapi);
+                today = DateTime.Now.ToString("yyyy-MM-dd");
+            }
+            try
+            {
+                LogFile = path + DateTime.Now.ToString("yyyyMMddHH") + ".log";
+                if (!File.Exists(LogFile))
+                {
+                    file.newFile(LogFile);
+                }
+                string status = "?Status=-1";
+                if (readsending)
+                    status = "?Status=10";// &id="+MsgId;
+                dynamic result = HttpHelper.GetWebApi(readapi + status);
+                if (result != null)
+                {
+                    string rowid = result.RowId;
+                    MailList mail = new MailList();
+                    mail.RowId = rowid;mail.Data = result;
+                    maillist.Insert(maillist.Count,mail);
+                    var where = new { RowId = rowid };
+                    HttpHelper.PostWebApi(sendapi, JsonConvert.SerializeObject(where), 18000);
+                }
+                if (sendstatus)
+                    sendmail();
+            }
+            catch (Exception ex)
+            {
+                file.addLine(LogFile, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：数据错误：" + ex.Message);
+                sendMailList.Items.Insert(0, ex.Message);
+            }
+        }
+
+        private void timer2_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (sendstatus)
+            {
+                sendmail();
+            }
+        }
+        public void like()
+        {
+            ;
+        }
+    }
+
+    public class MailList
+    {
+        public string RowId { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
